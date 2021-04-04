414ba30b6387181bd34383f6f56f56df40b5c8fc
Lucca <omnichrome@gmail.com>
2021-04-04 14:04:37 -0300

    Update port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const fs = require('fs');
 
 // port number for HTTP server
-const port = 2500;
+const port = 3000;
 
 // sendFile function - sends files in the HTTP response
 // INPUT:
