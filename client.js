aa8115adc0ee30811f93456c9f9be4264b95d2bb
Lucca <omnichrome@gmail.com>
2021-04-05 11:48:57 -0300

    Client-side JS logs new text to console

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 function changeText() {
   var newText = document.getElementById('inputField').value;
   document.getElementById('text').innerText = newText;
+  console.log('Text field now reads: "' + newText + '"');
 }
